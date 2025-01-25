e48f28f45412b0df8757ad401248a70dcaa3ac59
Tanmay Kumar Jha <161614297+TanmayKJha@users.noreply.github.com>
2025-01-25 17:28:53 +0530

    String Matching ,KMP

---
+++ String/String Matching in an Array.cpp
@@ -0,0 +1,23 @@
+//BRUTEFORCE 
+//TC -> O(N2*m) m--> size of string we are searching
+
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        int n = words.size();
+        vector<string> result;
+        for(int i =0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(i==j) continue;
+                if(words[j].find(words[i])!=string::npos){
+                  result.push_back(words[i]);
+                  break;
+                }
+                
+            }
+        }
+        return result;
+    }
+};
+
+////////////////////////////////////////////////////////OPTIMAL APPROACH USING KMP/////////////////////////////////////////////////////////////////////////////////////////////
