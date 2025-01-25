ec387aa0e221174c10465b07ccb71a8aa6cf74c9
Tanmay Kumar Jha <161614297+TanmayKJha@users.noreply.github.com>
2025-01-25 18:19:10 +0530

    Using XOR Properties, Hashmap

--- BIT MANUPULATION/Single Number.cpp
+++ BIT MANUPULATION/Single Number.cpp
@@ -9,3 +9,21 @@ public:
         
     }
 };
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//USING HASH MAP
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+
+        for(int i=0;i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(auto it : mp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
