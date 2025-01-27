fbc2b3342b08eef5301cb2e5287c7f3f680c79f0
Tanmay Kumar Jha <161614297+TanmayKJha@users.noreply.github.com>
2025-01-27 21:48:22 +0530

    Same as Same Tree

---
+++ Binary Tree.cpp/Symmetric Tree.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+
+    bool solve(TreeNode*L, TreeNode* R){
+
+        if(L==NULL && R==NULL){
+            return true;
+        }
+
+        if(L==NULL || R==NULL){
+            return false;
+        }
+        
+        if(L->val!=R->val){
+            return false;
+        }
+
+        return solve(L->left,R->right) && solve(L->right,R->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        
+        return solve(root->left,root->right);
+    }
+};
