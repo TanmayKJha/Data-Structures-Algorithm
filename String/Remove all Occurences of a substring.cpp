031dde67ee71405ddfafbbb2a70881c9bff253d7
Tanmay Kumar Jha <161614297+TanmayKJha@users.noreply.github.com>
2025-02-11 20:53:09 +0530

    string npos, erase,find function basic string manipulation

--- String/Remove all Occurences of a substring.cpp
+++ String/Remove all Occurences of a substring.cpp
@@ -1,1 +1,12 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int pos = s.find(part);
 
+        while(pos!=string::npos){
+            s.erase(pos,part.size());
+            pos = s.find(part);
+        }
+        return s;
+    }
+};
